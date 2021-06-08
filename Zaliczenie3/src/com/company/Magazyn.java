eee3e18b36d1216ad7154a5200b5f52e34ea05e5
Szymon Nowakowski <79590251+nowakowskiszymon@users.noreply.github.com>
2021-06-08 23:02:57 +0200

    Add files via upload

---
+++ Zaliczenie3/src/com/company/Magazyn.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.List;
+
+public class Magazyn {
+    private String Dostepnosc;
+    private String Pojemnosc;
+    List<Produkt> produktList;
+
+    public Magazyn() {
+    }
+
+    public void Aktualizuj_stan(Produkt){
+
+    }
+
+    public void Powiadamiaj_o_braku(){
+
+    }
+
+    public void Wyszuj_produkt(Produkt){
+
+    }
+}
